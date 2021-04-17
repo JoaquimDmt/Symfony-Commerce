0aeb343d424057b0fda319bb6e4e23aef411e289
Joaquim Dmt <joaquim.dimicode@users.noreply.github.com>
2021-04-17 13:18:54 +0200

    Project initialisation with composer

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController
+{
+
+    public function index(): Response
+    {
+        return new Response('Hello c moi');
+    }
+}
