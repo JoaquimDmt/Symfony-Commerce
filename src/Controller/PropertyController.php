37a54fa661daaf30b70b7f1af830d5539af99a7e
Joaquim Dmt <joaquim.dimicode@users.noreply.github.com>
2021-04-18 13:31:18 +0200

    Pagination + random items generation

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Doctrine\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -28,12 +30,20 @@ class PropertyController extends AbstractController
 
     /**
      * @Route("/biens", name="property.index")
+     * @param \Knp\Component\Pager\PaginatorInterface $paginator
+     * @param \Symfony\Component\HttpFoundation\Request $request
      * @return Response
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery(),
+            $request->query->getInt('page', 1),
+            12
+        );
         return $this->render('property/index.html.twig', [
-            'current_menu' => 'properties'
+            'current_menu' => 'properties',
+            'properties'   => $properties
         ]);
     }
 
---
+++ src/DataFixtures/PropertyFixture.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Property;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+
+class PropertyFixture extends Fixture
+{
+    # to generate random properties
+    # type in terminal: php bin/console doctrine:fixtures:load
+    # warning! it will remove everything from database
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 100; $i++) {
+            $property = new Property();
+            $property
+                ->setTitle($faker->words(3, true))
+                ->setDescription($faker->sentences(3, true))
+                ->setSurface($faker->numberBetween(20, 350))
+                ->setRooms($faker->numberBetween(2,20))
+                ->setBedrooms($faker->numberBetween(1, 8))
+                ->setFloor($faker->numberBetween(0, 10))
+                ->setPrice($faker->numberBetween(90000, 1000000))
+                ->setHeat($faker->numberBetween(0, count(Property::HEAT) - 1))
+                ->setCity($faker->city)
+                ->setAddress($faker->address)
+                ->setPostalCode($faker->postcode)
+                ->setSold(false);
+            $manager->persist($property);
+        }
+        $manager->flush();
+    }
+}
