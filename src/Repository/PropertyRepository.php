f57dc2591164254e250913f70e7a361f19f9faeb
Joaquim Dmt <joaquim.dimicode@users.noreply.github.com>
2021-04-18 14:14:40 +0200

    Search with filters (surface/budget)

--- src/Repository/PropertyRepository.php
+++ src/Repository/PropertyRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\Query;
 use Doctrine\ORM\QueryBuilder;
@@ -22,12 +23,36 @@ class PropertyRepository extends ServiceEntityRepository
     }
 
     /**
+     * @param \App\Entity\PropertySearch $search
      * @return Query
      */
-    public function findAllVisibleQuery(): Query
+    public function findAllVisibleQuery(PropertySearch $search): Query
     {
-        return $this->findVisibleQuery()
-            ->getQuery();
+        $query = $this->findVisibleQuery();
+
+        if ($search->getMaxPrice()) {
+            $query = $query
+                ->andWhere('p.price <= :maxprice')
+                ->setParameter('maxprice', $search->getMaxPrice());
+        }
+
+        if ($search->getMinSurface()) {
+            $query = $query
+                ->andWhere('p.surface >= :minsurface')
+                ->setParameter('minsurface', $search->getMinSurface());
+        }
+
+//        if ($search->getOptions()->count() > 0) {
+//            $k = 0;
+//            foreach($search->getOptions() as $option) {
+//                $k++;
+//                $query = $query
+//                    ->andWhere(":option$k MEMBER OF p.options")
+//                    ->setParameter("option$k", $option);
+//            }
+//        }
+
+        return $query->getQuery();
     }
 
     /**
